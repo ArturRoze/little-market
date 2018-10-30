6e6cc42abbad1f295c6721b53b7aeea054a0f9ba
ArtyrRoze <a.r.c.h.y@rambler.ru>
2018-10-30 19:32:44 +0200

    add getter&setter to BasicUser

--- src/main/java/com/app/model/BasicUser.java
+++ src/main/java/com/app/model/BasicUser.java
@@ -17,6 +17,54 @@ abstract class BasicUser {
     @Column (name = "registration_date")
     private Timestamp registrationDate;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Timestamp getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Timestamp registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "BasicUser{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", registrationDate=" + registrationDate +
+                '}';
+    }
 }
